7320772210fca22911caf0b19bf325a192d6e22d
prabath0061 <prabath0061@gmail.com>
2024-06-09 23:09:07 +0530

    Revert "add partner payment template"
    This reverts commit f512d804b530c7fb65bfa5c8d472cc835cac2994.

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const pug = require('pug');
 const fs = require('fs');
 
 // Compile the Pug template to HTML
-const compiledFunction = pug.compileFile('email-template-2.pug');
+const compiledFunction = pug.compileFile('email-template.pug');
 
 // Write the HTML to a file
-fs.writeFileSync('email-template-2.html', compiledFunction());
+fs.writeFileSync('email-template.html', compiledFunction());
