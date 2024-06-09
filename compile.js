78b3a835401288c542120afdff70c063932c53f5
kalum.galmangoda <kalum.galmangoda@gmail.com>
2024-06-09 11:45:38 +0530

    fix: Add calander

--- compile.js
+++ compile.js
@@ -1,8 +1,10 @@
+//node compile.js or nodemon compile.js -e js,pug
+
 const pug = require('pug');
 const fs = require('fs');
 
 // Compile the Pug template to HTML
-const compiledFunction = pug.compileFile('email-template.pug');
+const compiledFunction = pug.compileFile('email-template-1.pug');
 
 // Write the HTML to a file
-fs.writeFileSync('email-template.html', compiledFunction());
+fs.writeFileSync('email-template-1.html', compiledFunction());
