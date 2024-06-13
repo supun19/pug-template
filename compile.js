4052fb511012f92d1e419ed0ee72ad00df5ee565
prabath0061 <prabath0061@gmail.com>
2024-06-12 22:56:02 -0700

    update

--- compile.js
+++ compile.js
@@ -1,10 +1,42 @@
 //node compile.js or nodemon compile.js -e js,pug
 
-const pug = require('pug');
-const fs = require('fs');
+const pug = require("pug");
+const fs = require("fs");
+
+const param = {
+  name: "IoT Project Factory",
+  impressionCountOfPosts: [
+    {
+      id: "46A3twh0Qvc6GehP8A5X",
+      totalImpressions: 3,
+      totalEngagement: 0,
+    },
+  ],
+  growOfPost: {
+    instagram: {
+      impressionInstragrameGrowth: 3,
+      reachInstragrameGrowth: NaN,
+      followsInstragrameGrowth: NaN,
+      postCountInstragrameGrowth: 1,
+    },
+    linkdin: {
+      impressionLinkdinGrowth: "Set Up!",
+      reachLinkdinGrowth: "Set Up!",
+      followsLinkdinGrowth: "Set Up!",
+      postCountLinkdinGrowth: "Set Up!",
+    },
+    facebook: {
+      impressionFacebookGrowth: "Set Up!",
+      reachFacebookGrowth: "Set Up!",
+      followsFacebookGrowth: "Set Up!",
+      postCountFacebookGrowth: "Set Up!",
+    },
+  },
+  formattedMonthDate: "June 2024",
+};
 
 // Compile the Pug template to HTML
-const compiledFunction = pug.compileFile('email-template-3.pug');
+const compiledFunction = pug.compileFile("emailTemplate.pug");
 
 // Write the HTML to a file
-fs.writeFileSync('email-template-3.html', compiledFunction());
+fs.writeFileSync("emailTemplate.html", compiledFunction(param));
