67fafbac8f2acc0980f055bcdc8bb27320611008
prabath0061 <prabath0061@gmail.com>
2024-06-24 22:43:40 +0530

    change performance table style

--- compile.js
+++ compile.js
@@ -4,36 +4,43 @@ const pug = require("pug");
 const fs = require("fs");
 
 const param = {
-  name: "IoT Project Factory",
+  name: 'IoT Project Factory',
   impressionCountOfPosts: [
     {
-      id: "46A3twh0Qvc6GehP8A5X",
-      totalImpressions: 3,
-      totalEngagement: 0,
-    },
+      id: 'bXv2xDjf2zzF7XFTHmsk',
+      totalImpressions: 5,
+      totalEngagement: 1
+    }
   ],
   growOfPost: {
     instagram: {
-      impressionInstragrameGrowth: 3,
-      reachInstragrameGrowth: NaN,
-      followsInstragrameGrowth: NaN,
-      postCountInstragrameGrowth: 1,
-    },
-    linkdin: {
-      impressionLinkdinGrowth: "Set Up!",
-      reachLinkdinGrowth: "Set Up!",
-      followsLinkdinGrowth: "Set Up!",
-      postCountLinkdinGrowth: "Set Up!",
+      followers: 5,
+      followersGrowth: '100%',
+      impressions: 0,
+      impressionsGrowth: '0%',
+      reach: 26,
+      reachGrowth: '100%',
+      engagementRate: '2.9%'
     },
     facebook: {
-      impressionFacebookGrowth: "Set Up!",
-      reachFacebookGrowth: "Set Up!",
-      followsFacebookGrowth: "Set Up!",
-      postCountFacebookGrowth: "Set Up!",
-    },
+      followers: 1,
+      followersGrowth: '100%',
+      impressions: 0,
+      impressionsGrowth: '0%',
+      reach: 1,
+      reachGrowth: '100%',
+      engagementRate: '0%'
+    }
   },
-  formattedMonthDate: "June 2024",
-};
+  formattedMonthDate: 'June 2024',
+  followersGrowthUrl: 'https://realtor-storage233045-dev.s3.us-east-2.amazonaws.com/d35146af-975e-4258-826d-a6714544af52/1719206137610/followers.png',
+  impressionGrowthUrl: 'https://realtor-storage233045-dev.s3.us-east-2.amazonaws.com/d35146af-975e-4258-826d-a6714544af52/1719206145412/impressions.png',
+  reachGrowthUrl: 'https://realtor-storage233045-dev.s3.us-east-2.amazonaws.com/d35146af-975e-4258-826d-a6714544af52/1719206150202/reach.png',
+  engagementGrowthUrl: 'https://realtor-storage233045-dev.s3.us-east-2.amazonaws.com/d35146af-975e-4258-826d-a6714544af52/1719206156436/engagement.png',
+  filterdMediaUrls: [
+    'https://realtor-storage233045-dev.s3.us-east-2.amazonaws.com/jignesh78bmw@gmail.com/2024-06-18/template-image-5-94d77b2b2a0a2a98/789ee3ae-fb0f-4117-a2c4-0cdb00753b1f'
+  ]
+}
 
 // Compile the Pug template to HTML
 const compiledFunction = pug.compileFile("emailTemplate.pug");
